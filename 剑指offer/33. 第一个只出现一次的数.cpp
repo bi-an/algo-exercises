b0c6593cec24af1a23b4abfd1e680c787edca360
bi_an <2608120872@qq.com>
2019-03-20 15:34:46 +0800

    第一个只出现一次的字符，补充注释

--- 剑指offer/33. 第一个只出现一次的数.cpp
+++ 剑指offer/33. 第一个只出现一次的数.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 	int FirstNotRepeatingChar(string str) {
 		if (str.empty()) return -1;
-		const int tableSize = 256;
+		const int tableSize = 256; //由于char类型是8bit，所以一共可以表示256个字符
 		unsigned int hashTable[tableSize] = {0};
 		for (size_t i = 0; i < str.size(); i++) {
 			hashTable[str[i]]++;
