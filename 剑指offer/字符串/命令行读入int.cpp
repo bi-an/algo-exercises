e086d07794d5d20eca2fd16899e3bb501d90c9bc
bi-an <2608120872@qq.com>
2019-03-26 22:57:13 +0800

    命令行读入

---
+++ 剑指offer/字符串/命令行读入int.cpp
@@ -0,0 +1,58 @@
+// 空格分隔 换行结束
+
+
+// 1、scantf
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	while (scanf("%d", &x) != EOF) {
+		printf("%d ", x);
+	}
+
+	return 0;
+}
+
+
+
+// 2、stringstream
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+	string str;
+	getline(cin, str);
+
+	stringstream ss(str);
+
+	int x;
+	while (ss >> x) {
+		printf("%d ", x);
+	}
+
+	return 0;
+}
+
+
+
+// 3、推荐写法：getline
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	while (getline(cin, str, ' ')) { // geline指定分隔符
+		if(!str.empty())
+			printf("%s ", str.c_str());
+	}
+
+	return 0;
+}
+
