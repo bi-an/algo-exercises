1e9f57e1a4eabb01299dd032bcb4fc2b5fafb30c
bi-an <2608120872@qq.com>
2019-03-29 18:53:33 +0800

    注释：6.旋转数组，好像有错误
    Signed-off-by: bi-an <2608120872@qq.com>

--- 剑指offer/6. 旋转数组的最小数字.cpp
+++ 剑指offer/6. 旋转数组的最小数字.cpp
@@ -1,3 +1,9 @@
+// 面试题11：旋转数组的最小数字
+// 题目：把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+// 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如数组
+// {3, 4, 5, 1, 2}为{1, 2, 3, 4, 5}的一个旋转，该数组的最小值为1。
+
+
 //本算法错误：当数组没有旋转时，输出错误。（但是能通过牛客网测试，应该是bug）
 class Solution {
 public:
@@ -40,14 +46,14 @@ public:
 		if (rotateArray.empty()) return 0;
 		int left = 0, right = rotateArray.size() - 1;
 		//if (right == 0) return rotateArray[0];
-		//当左右相等时，如：1,0,1,1,1和1,1,1,0,1，是没法判断下一步min属于哪个区间，应该顺序寻找。
-		if (rotateArray[left] == rotateArray[right]) {
+		//当左右相等时，如：1,0,1,1,1和1,1,1,0,1，没法判断下一步min属于哪个区间，应该顺序寻找。
+		if (rotateArray[left] == rotateArray[right]) { // Question: 好像有错误？
 			while (left < right) {
 				if (rotateArray[right] < rotateArray[right - 1])
 					return rotateArray[right];
 				right--;
 			}
-			return rotateArray[0];
+			return rotateArray[0]; //如果只有一个元素或者本身就是顺序的，返回第一个数
 		}
 		//二分法
 		int middle = 0;
