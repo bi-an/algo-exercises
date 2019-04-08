b11f730322cc6ed2fd84950c920efc901a67aa30
bi-an <2608120872@qq.com>
2019-04-08 17:56:32 +0800

    AVLTree.h部分实现

--- 剑指offer/二叉树/二叉查找树/BinarySearchTree.h
+++ 剑指offer/二叉树/二叉查找树/BinarySearchTree.h
@@ -1,77 +1,153 @@
 #pragma once
-#include "BinaryNode.h"
 
-template<typename Comparable> class BinarySearchTree{
+template<typename T> class BinarySearchTree{
 public:
-	BinarySearchTree();
-	BinarySearchTree(const BinarySearchTree& rhs);
-	~BinarySearchTree();
+	BinarySearchTree(){}
+	BinarySearchTree(const BinarySearchTree& rhs){
 
-	const Comparable& findMin() const;
-	const Comparable& findMax() const;
-	bool contains(const BinarySearchTree& x) const;
+	}
+	~BinarySearchTree(){
+		makeEmpty();
+	}
+
+	const T& findMin() const; // 什么情况下不能返回引用？
+	const T& findMax() const;
+	bool contains(const T& x) const;
 	bool isEmpty() const;
 	void printTree() const;
 
 	void makeEmpty();
-	void insert(const Comparable& x);
-	void remove(const Comparable& x);
+	void insert(const T& x);
+	void remove(const T& x);
 
-	const BinarySearchTree& operator=(const BinarySearchTree & rhs); // 为什么要返回const引用呢，这里的const和引用似乎都没有起到作用
+	const BinarySearchTree& operator=(const BinarySearchTree& rhs);
 
 private:
-	BinaryNode<Comparable> *root;
+	struct BinaryNode{
+		T val;
+		BinaryNode *left;
+		BinaryNode *right;
+	};	
 
-	void insert(const Comparable& x, BinaryNode<Comparable>* &t) const; // 即使是const成员函数，也可以通过传入引用或指针来改变成员变量
-	void remove(const Comparable& x, BinaryNode<Comparable>* &t) const;
-	BinaryNode<Comparable>* findMin(BinaryNode<Comparable>* t) const;
-	BinaryNode<Comparable>* findMax(BinaryNode<Comparable>* t) const;
-	bool contains(const Comparable& x, BinaryNode<Comparable>* t) const;
-	void makeEmpty(BinaryNode<Comparable>* &t) const;
-	void printTree(BinaryNode<Comparable> *t) const;
-	BinaryNode<Comparable>* clone(BinaryNode<Comparable> *t) const;
+	BinaryNode *root;
+
+	void insert(const T& x, BinaryNode *&t) const; // 用引用参数可以修改root
+	void remove(const T& x, BinaryNode *&t) const;
+	BinaryNode* findMin(BinaryNode *t) const;
+	BinaryNode* findMax(BinaryNode *t) const;
+	bool contains(const T& x, BinaryNode* t) const;
+	void makeEmpty(BinaryNode *&t) const;
+	void printTree(BinaryNode *t) const;
+	BinaryNode* clone(BinaryNode *t) const;
 };
 
-template<typename Comparable> BinarySearchTree<Comparable>::BinarySearchTree() {
-
+template<typename T> const T& BinarySearchTree<T>::findMin() const{
+	return findMin(root);
+}
+template<typename T> const T& BinarySearchTree<T>::findMax() const{
+	return findMax(root);
+}
+template<typename T> bool BinarySearchTree<T>::contains(const T& x) const{
+	return contains(x,root);
+}
+template<typename T> bool BinarySearchTree<T>::isEmpty() const{
+	// TODO
+	return root==nullptr;
+}
+template<typename T> void BinarySearchTree<T>::printTree() const{
+	printTree(root);
+}
+template<typename T> void BinarySearchTree<T>::makeEmpty(){
+	makeEmpty(root);
+}
+template<typename T> void BinarySearchTree<T>::insert(const T& x){
+	insert(x,root);
+}
+template<typename T> void BinarySearchTree<T>::remove(const T& x){
+	remove(x,root);
+}
+template<typename T> const BinarySearchTree<T>& operator=(const BinarySearchTree& rhs){
+	if(this!=&rhs){ // 自我复制检测
+		makeEmpty();
+		root=clone(rhs.root);		
+	}
+	return *this;
 }
 
-template<typename Comparable> BinarySearchTree<Comparable>::~BinarySearchTree() {
-
-}
-
-template<typename Comparable> const Comparable& BinarySearchTree<Comparable>::findMin() const{
-	return *root; // TODO
-}
-
-template<typename Comparable> const Comparable& BinarySearchTree<Comparable>::findMax() const{
-	return *root; // TODO
-}
-
-template<typename Comparable> bool BinarySearchTree<Comparable>::contains(const BinarySearchTree& x) const{
-	return true; // TODO
-}
-
-template<typename Comparable> bool BinarySearchTree<Comparable>::isEmpty() const{
-	return true; // TODO
-}
-
-template<typename Comparable> void BinarySearchTree<Comparable>::printTree() const{
-	return true; // TODO
-}
-
-// todo: 似乎有错，根本没有将节点连接起来
-template<typename Comparable> void BinarySearchTree<Comparable>::insert(const Comparable& x, BinaryNode<Comparable>* &t) const {
+// 保证永远不会插入重复项
+// 如果要允许重复项，则需要修改bst树的定义，例如：左子树所有节点值小于根，右子树的所有节点大于或等于根。
+// 但是getEntry和remove会有歧义，需要类的定义者来指明。
+template<typename T> void BinarySearchTree<T>::insert(const T& x, BinaryNode *t) const{
 	if(t==nullptr)
-		t=new BinaryNode(x,nullptr,nullptr);
-	else if(x < t->val)
-		insert(x, t->left);
-	else if(x > t->val)
-		insert(x, t->right);
+		t=new BinaryNode(x);
+	else if(x<t->val)
+		insert(x,t->left);
+	else if(x>t->val)
+		insert(x,t->right);
 	else
 		; // Duplicate; do nothing
 }
-
-template<typename Comparable> void BinarySearchTree<Comparable>::insert(const Comparable& x){
-	insert(x,root);
+// 要删除节点N：
+// 找到节点左子树的最右孩子R
+// 用R的值替换N的值
+// 删除节点R
+// 同理：也可以删除右子树的最左节点
+template<typename T> void BinarySearchTree<T>::remove(const T& x, BinaryNode *t) const{
+	if(t==nullptr)
+		return;
+	else if(x<t->val)
+		remove(x,t->left);
+	else if(x>t->val)
+		remove(x,t->right);
+	else if(t->left!=nullptr || t->right!=nullptr){ // x==t->val and the node has two children
+		t->val=findMin(x,t->right)->val; // 把当前节点的值用右子树的最小值来替换，并且从右子树的根开始往下寻找并删除右子树的这个最小值节点，对右子树进行了两次查找，所以效率不高
+		remove(t->val,t->right); // 第二次查找并删除，因为右子树的最小节点不可能有左孩子，所以第二次删除等于删除只有一个孩子节点的情况；效率不高，应该用removeMin() / removeFromRoot()来代替
+	}
+	else{ // x==t->val and the node only has one children
+		BinaryNode *oldNode=t;
+		t=(t->left!=nullptr)?t->left:t->right;
+		delete oldNode; // 如果删除的次数不多或者有重复项时，一般使用lazy deletion: 不删除节点，而是做一个删除记号.
+		// 在没有删除或者只是懒惰删除时，可以证明所有二叉查找树都是等可能出现的。
+	}
+}
+template<typename> BinaryNode* BinarySearchTree<T>::findMin(BinaryNode *t) const{
+	if(t==nullptr)
+		return nullptr;
+	else if(t->left==nullptr)
+		return t;
+	else
+		findMin(t->left);
+}
+template<typename> BinaryNode* BinarySearchTree<T>::findMax(BinaryNode *t) const{
+	if(t!=nullptr)
+		while(t->right!=nullptr)
+			t=t->right;
+	return t;
+}
+template<typename> bool BinarySearchTree<T>::contains(const T& x, BinaryNode* t) const{
+	if(t==nullptr)
+		return false;
+	else if(x<t->left)
+		return contains(x,t->left);
+	else if(x>t->right)
+		return contains(x,t->right);
+	else
+		return true; // Match
+}
+template<typename> void BinarySearchTree<T>::makeEmpty(BinaryNode *&t) const{
+	if(t!=nullptr){
+		makeEmpty(t->left);
+		makeEmpty(t->right);
+		delete t;
+	}
+	t=nullptr;
+}
+template<typename> void BinarySearchTree<T>::printTree(BinaryNode *t) const{
+	
+}
+// Deep copy
+template<typename> BinaryNode* BinarySearchTree<T>::clone(BinaryNode *t) const{
+	if(t==nullptr)
+		return nullptr;
+	return new BinaryNode(t->val, clone(t->left), clone(t->right));
 }
---
+++ 剑指offer/二叉树/平衡查找树/AVLTree.h
@@ -0,0 +1,91 @@
+#pragma once
+
+// AVL树是平衡的BST树(任何节点都是平衡的)
+// 高度是O(logN)
+template<typename T> class AVLTree{
+public:
+	AVLTree(){}
+	AVLTree(const AVLTree& rhs){
+
+	}
+	~AVLTree(){
+		makeEmpty();
+	}
+
+	const T& findMin() const; // 什么情况下不能返回引用?
+	const T& findMax() const;
+	bool contains(const T& x) const;
+	bool isEmpty() const;
+	void printTree() const;
+
+	void makeEmpty();
+	void insert(const T& x);
+	void remove(const T& x);
+	int getHeight() const;
+
+	const AVLTree& operator=(const AVLTree& rhs);
+
+private:
+	struct AVLNode{
+		T val;
+		AVLNode *left;
+		AVLNode *right;
+		int height;
+	};	
+
+	AVLNode *root;
+
+	int getHeight(AVLNode *t) const;
+	AVLNode* rotateLeft(AVLNode * nodeN); // 可以用*&
+	AVLNode* rotateRight(AVLNode * nodeN);
+	AVLNode* rotateLeftRight(AVLNode *nodeN);
+	AVLNode* rotateRightLeft(AVLNode *nodeN);
+
+	void insert(const T& x, AVLNode *&t) const; // 用引用参数可以修改root
+	void remove(const T& x, AVLNode *&t) const;
+	AVLNode* findMin(AVLNode *t) const;
+	AVLNode* findMax(AVLNode *t) const;
+	bool contains(const T& x, AVLNode* t) const;
+	void makeEmpty(AVLNode *&t) const;
+	void printTree(AVLNode *t) const;
+	AVLNode* clone(AVLNode *t) const;
+
+};
+
+
+// 找到第一个不平衡的节点nodeN
+// nodeC=nodeN的左孩子
+// nodeN的左孩子=nodeC的右孩子
+// nodeC的右孩子=nodeN
+// return nodeC
+template<typename T> AVLNode* AVLTree<T>::rotateRight(AVLNode *nodeN){
+	AVLNode *nodeC=nodeN->left;
+	nodeN->left=nodeC->right;
+	nodeC->right=nodeN;
+	// 高度更新
+	nodeC->height=std::max(getHeight(nodeC->left),getHeight(nodeC->right))+1; // 有没有必要这么求？直接用getHeight(nodeC)行不行？
+	nodeN->height=std::max(getHeight(nodeN->left),nodeC->height)+1; // nodeN的右孩子是nodeC
+	return nodeC;
+}
+
+template<typename T> AVLNode* AVLTree<T>::rotateLeft(AVLNode *nodeN){
+	AVLNode *nodeC=nodeN->right;
+	nodeN->right=nodeC->left;
+	nodeC->left=nodeN;
+	// 高度更新
+	// TODO
+	return nodeC;
+}
+
+template<typename T> AVLNode* AVLTree<T>::rotateRightLeft(AVLNode *nodeN){
+	
+}
+
+// 对于空树，不能访问t->height，所以使用getHeight()函数更安全
+template<typename T> int AVLTree<T>::getHeight(AVLNode *t) const{
+	return t==nullptr?0:t->height;
+}
+
+template<typename T> void AVLTree<T>::insert(const T& x, AVLNode*& t) const{
+	
+}
