d6f4ad04a73bd62c6c80d36e977eb355950a04c7
bi-an <2608120872@qq.com>
2019-05-31 15:18:41 +0800

    字符流中的第一个出现一次的字符

--- 剑指offer/100. 字符流中第一个只出现一次的字符.cpp
+++ 剑指offer/100. 字符流中第一个只出现一次的字符.cpp
@@ -3,15 +3,15 @@ class Solution
 public:
 	Solution() :index(0) {
 		for (int i = 0; i < 256; i++)
-			occurence[i] = -1;
+			pos[i] = -1;
 	}
 	//Insert one char from stringstream
 	void Insert(char ch)
 	{
-		if (occurence[ch] == -1)
-			occurence[ch] = index;
-		else if (occurence[ch] >= 0)
-			occurence[ch] = -2;
+		if (pos[ch] == -1)
+			pos[ch] = index;
+		else if (pos[ch] >= 0)
+			pos[ch] = -2;
 		++index;
 	}
 	//return the first appearence once char in current stringstream
@@ -20,20 +20,20 @@ public:
 		char ch = '#';
 		int minIndex = std::numeric_limits<int>::max();
 		for (int i = 0; i < 256; i++) {
-			if (occurence[i] >= 0 && occurence[i] < minIndex) {
+			if (pos[i] >= 0 && pos[i] < minIndex) {
 				ch = (char)i;
-				minIndex = occurence[i];
+				minIndex = pos[i];
 			}
 		}
 		return ch;
 	}
 
 private:
-	// occurence[i]: i表示字符的ascii码
-	// occurence[i]=-1: 该字符没有出现
-	// occurence[i]=-2: 该字符不止出现一次
-	// occurence[i]>=0: 该字符只出现了一次
-	int occurence[256];
+	// pos[i]: i表示字符的ascii码
+	// pos[i]=-1: 该字符没有出现
+	// pos[i]=-2: 该字符不止出现一次
+	// pos[i]>=0: 该字符只出现了一次
+	int pos[256]; // 下标i对应的字符 (char)i 在字符流中的位置
 	int index; //记录字符流的下个位置
 
 };
