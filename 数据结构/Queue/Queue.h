6cc23932f7a43f347a9f208332e03a8f8db3f620
bi-an <2608120872@qq.com>
2019-06-01 18:52:29 +0800

    Queue.h

--- 数据结构/Queue/Queue.h
+++ 数据结构/Queue/Queue.h
@@ -5,20 +5,22 @@
 template<typename T>
 class Queue {
 public:
-	Queue() :data(new T[1]), m_front(0), m_back(0), m_capacity(1), m_size(0) {}
+	Queue() :data(new T[1]), m_front(0), m_back(0), m_capacity(0), m_size(0) {}
 	~Queue() {
-		delete[] data; // for(int i=0;i<m_capacity;i++) delete (data+i); 出错？这个new[size]的size信息到底存放在哪里？
+		delete[] data;
 	}
 	void push(T val) {
-		data[m_back] = val;
-		m_size++;
 		if (m_size >= m_capacity) // 此时要预留空间以插入新元素，所以即使相等也需要扩容
 			resize();
+		data[m_back] = val;
+		m_size++;
 		m_back++;
-		m_back %= m_capacity;
+		if(m_size!=m_capacity) // 由于m_back指向后一个元素，如果队列元素个数与数组容量相等，m_back应该指向数组之后的位置，否则循环转移到数组首位
+			m_back %= m_capacity;
+		// if(m_size!=m_capacity && m_back==m_capacity)
+		//		m_back=0; // 似乎与上面的是一样的
 	}
 	void pop() {
-		// m_front >= 0 && m_front < m_capacity 的判断似乎不需要
 		assert(m_front != m_back); // 队列为空的标志：m_front==m_back
 		m_front++;
 		m_front %= m_capacity;
@@ -33,12 +35,6 @@ public:
 		return data[m_front];
 	}
 
-	// 这个back函数实际上不需要
-	T back() {
-		assert(m_front != m_back);
-		return data[m_back];
-	}
-
 	int size() {
 		return m_size;
 	}
@@ -49,13 +45,15 @@ public:
 
 private:
 	T *data;
-	int m_front;
+	int m_front; // 左闭右开区间，m_back指向下一个元素
 	int m_back;
 	int m_capacity;
 	int m_size;
 
 	void resize() {
 		int sz = m_capacity * 2;
+		if (sz == 0) // 因为初始时，m_capacity=0
+			sz = 1;
 		T *data_new = new T[sz];
 		if (m_back < m_front) {
 			for (int i = m_front; i < m_capacity; i++) // 此时m_capacity==m_size
@@ -67,7 +65,8 @@ private:
 			for (int i = 0; i < m_capacity; i++)
 				data_new[i] = data[i];
 		}
-		delete[] data;
+		if(data!=nullptr) // 允许delete nullptr; 但是不允许delete[] nullptr;
+			delete[] data;
 		m_capacity = sz;
 		data = data_new;
 	}
