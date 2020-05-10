ae61e92bfaa84adda39e7f6cd442b0da70ab17d9
zzg <2608120872@qq.com>
2020-05-10 18:08:58 +0800

    自己重写“删除多余空格.cc”

--- 剑指offer/字符串/删除多余空格.cc
+++ 剑指offer/字符串/删除多余空格.cc
@@ -3,84 +3,73 @@
  * 如果有标点符号，删除标点之前的空格
  */
 
-// C++ program to implement custom trim() function
-#include <algorithm>
 #include <iostream>
 using namespace std;
 
-// Function to in-place trim all spaces in the
-// string such that all words should contain only
-// a single space between them.
-void removeSpaces(string &str) {
-  // n is length of the original string
+void removeSpaces(string &str)
+{
   int n = str.length();
-
-  // i points to next position to be filled in
-  // output string/ j points to next character
-  // in the original stringi
-  int i = 0, j = -1;
-
-  // flag that sets to true is space is found
+  int i = 0, j = -1; // i写指针，j读指针
   bool spaceFound = false;
-
-  // Handles leading spaces
+  // j定位到第一个非空字符
   while (++j < n && str[j] == ' ')
-    ;
-
-  // read all characters of original string
-  while (j < n) {
-    // if current characters is non-space
-    if (str[j] != ' ') {
-      // remove preceding spaces before dot,
-      // comma & question mark
-      if ((str[j] == '.' || str[j] == ',' || str[j] == '?') && i - 1 >= 0 &&
-          str[i - 1] == ' ')
-        str[i - 1] = str[j++];
-
+  {
+  }
+  // 标点符号列表
+  string punctuation_marks = ",.?!";
+  while (j < n)
+  {
+    if (str[j] != ' ')
+    {
+      // 如果读到标点符号，删除前面写入的空格
+      if (punctuation_marks.find_first_of(str[j]) != std::string::npos && i - 1 >= 0 && str[i - 1] == ' ')
+      {
+        // swap的好处是不增加新的符号，不然很难判断尾部的空格
+        swap(str[i - 1], str[j++]); // 这是删除，所以写指针不动
+      }
       else
-        // copy current character at index i
-        // and increment both i and j
-        //        str[i++] = str[j++];
+      {
         swap(str[i++], str[j++]);
-
-      // set space flag to false when any
-      // non-space character is found
+      }
       spaceFound = false;
     }
-    // if current character is a space
-    else if (str[j++] == ' ') {
-      // If space is encountered for the first
-      // time after a word, put one space in the
-      // output and set space flag to true
-      if (!spaceFound) {
-        str[i++] = ' ';
+    else
+    {
+      // 如果上一次没有空格，那么写入一个空格
+      if (!spaceFound)
+      {
+        swap(str[i++], str[j]);
+        // str[i++] = ' ';
         spaceFound = true;
       }
+      // 如果本次和上次读到的都是空格，那么写指针不动，读指针后移
+      ++j;
     }
   }
-
-  // Remove trailing spaces
-  //  if (i <= 1)
-  //    str.erase(str.begin() + i, str.end());
-  //  else
-  //    str.erase(str.begin() + i - 1, str.end());
+  // 删除尾部的空格
   i = n - 1;
-  while (i < n && str[i] == ' ') i--;
+  while (i < n && str[i] == ' ')
+    --i;
   str.erase(str.begin() + i + 1, str.end());
 }
 
-void test(int id, string in, string out) {
+/// 测试
+void test(int id, string in, string out)
+{
   cout << id << ":";
   removeSpaces(in);
-  if (in == out) {
+  if (in == out)
+  {
     cout << "right" << endl;
-  } else {
+  }
+  else
+  {
     cout << "false" << endl;
   }
 }
 
-// Driver Code
-int main() {
+int main()
+{
   test(1, "I", "I");
   test(2, "I ", "I");
   test(3, " I", "I");
@@ -98,4 +87,3 @@ int main() {
 
   return 0;
 }
-
