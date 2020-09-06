7bd6fc0c529f138f953c1f39407b048789512ebf
zzg <2608120872@qq.com>
2020-09-06 08:34:21 +0800

    2019春字节第6题

---
+++ 公司真题/2019春招：字节跳动/6.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int num[4];
+  memset(num, 0, sizeof(num));
+
+  int total, coins = 0;
+  cin >> total;
+  total = 1024 - total;
+
+  if (total >= 64)
+  {
+    coins += total / 64;
+    total -= total / 64 * 64;
+  }
+  if (total >= 16)
+  {
+    coins += total / 16;
+    total -= total / 16 * 16;
+  }
+  if (total >= 4)
+  {
+    coins += total / 4;
+    total -= total / 4 * 4;
+  }
+  if (total >= 1)
+  {
+    coins += total;
+  }
+
+  cout << coins << endl;
+
+  return 0;
+}
