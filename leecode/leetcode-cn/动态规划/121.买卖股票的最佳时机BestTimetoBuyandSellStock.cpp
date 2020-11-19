12517a2d13243a98025c26ab9bed9b7cffce2be3
zzg <2608120872@qq.com>
2020-11-19 10:36:14 +0800

    leetcode-cn/动态规划/121.买卖股票的最佳时机：增加自己的解法

--- leecode/leetcode-cn/动态规划/121.买卖股票的最佳时机BestTimetoBuyandSellStock.cpp
+++ leecode/leetcode-cn/动态规划/121.买卖股票的最佳时机BestTimetoBuyandSellStock.cpp
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2020-10-15 14:55:18
+ * @LastEditTime: 2020-11-19 10:34:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \leecode\leetcode-cn\动态规划\121. 买卖股票的最佳时机 Best Time to Buy and Sell Stock.cpp
+ */
+/*
 Say you have an array for which the ith element is the price of a given stock on day i.
 
 If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
@@ -39,4 +47,23 @@ class Solution
     }
     return max_profit;
   }
+};
+
+
+// @Athor zzg
+class Solution_2 {
+public:
+    int maxProfit(vector<int>& prices) {
+		if(prices.empty()) return 0;
+        int cur_min = prices[0];
+        int res = 0;
+        for(int i = 0;i<prices.size();i++){
+            int diff = prices[i] - cur_min;
+            if(diff > res)
+                res = diff;
+            if(diff < 0)
+                cur_min = prices[i];
+        }
+        return res;
+    }
 };
