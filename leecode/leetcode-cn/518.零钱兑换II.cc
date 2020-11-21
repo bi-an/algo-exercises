153634e739c1a8aa9ae49551a3456a58593dbc62
zzg <2608120872@qq.com>
2020-11-21 16:27:47 +0800

    leetcode-cn/518.零钱兑换II：补充注释

--- leecode/leetcode-cn/518.零钱兑换II.cc
+++ leecode/leetcode-cn/518.零钱兑换II.cc
@@ -1,14 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 /**
  * @动态规划
  *   dp[i][j] 使用前i种面额硬币恰好凑成总金额为j的总组合数
  *  （1）不使用第i种面额
  *      dp[i-1][j]
  *  （2）使用第i种面额
- *      dp[i][j-coins[i]]，当j-coin>=0
- *    说明：使用了第i种面额凑成总额j的组合数，与使用了第i种面额凑成总额j-coins[i]是相同的，TOOD
+ *      dp[i][j-coins[i]]，当j-coins[i]>=0
+ *    说明：由于一定使用了第i种面额的至少一个，那么此时的组合数与减去一个这个面额的组合数是一样的。
+ *      如果j<coins[i]，说明不能使用这种面额，直接跳过即可（也就是对应的组合数不能加上去）。
+ *      动态规划使用的是递归思想，只要能准确地推出递推式和base case，就一定能算出来，至于这个递推式的形式可以有多种，每一种都是可行的。
  *  由于是求总组合数，所以这两种情况需要相加：
- *      dp[i][j] = dp[i-1][j] + dp[i][j-coin]
+ *      dp[i][j] = dp[i-1][j] + dp[i][j-coins[i]]
  * 
+ *  base case:
+ *      dp[0][j] = 0
+ *      dp[i][0] = 1，使用0个硬币就可以
  */
 
 class Solution {
@@ -16,7 +24,7 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
-		// 递推式dp[i][j-coins[i]]，如果j-coins[i]==0，此时应该恰好有一种方案
+		// 递推式dp[i][j-coins[i]]，如果j-coins[i]==0，此时应该恰好有一种方案:即使用coins[i]
         for(int i=0;i<=n;i++)
             dp[i][0] = 1; // 注意
 
@@ -30,5 +38,13 @@ public:
         }
 
         return dp[n][amount];
+    }
+};
+
+
+class Solution_2 {
+public:
+    int change(int amount, vector<int>& coins) {
+        
     }
 };
