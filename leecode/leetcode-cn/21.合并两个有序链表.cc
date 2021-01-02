21a40c1675da44ce0bfc4225c2df232602e512af
bi_an:desktop <2608120872@qq.com>
2021-01-02 18:24:03 +0800

    leetcode-cn/21.合并两个有序链表

---
+++ leecode/leetcode-cn/21.合并两个有序链表.cc
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==nullptr) return l2;
+        if(l2==nullptr) return l1;
+        if(l1->val > l2->val)
+            swap(l1, l2); // 保证l1是最小节点
+        ListNode* head = mergeTwoLists(l1->next, l2); // 返回子链表的头
+        l1->next = head;
+        return l1;
+    }
+};
