14378acc774c73c98bf4e109276274f36dad67b8
bi-an <2608120872@qq.com>
2021-07-31 11:29:09 +0800

    leetcode-cn/863.二叉树中所有距离为K的结点

---
+++ leecode/leetcode-cn/863.二叉树中所有距离为K的结点.cc
@@ -0,0 +1,91 @@
+/**
+ * 给定一个二叉树（具有根结点 root）， 一个目标结点 target ，和一个整数值 K 。
+
+返回到目标结点 target 距离为 K 的所有结点的值的列表。 答案可以以任何顺序返回。
+
+ 
+示例 1：
+
+输入：root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
+输出：[7,4,1]
+解释：
+所求结点为与目标结点（值为 5）距离为 2 的结点，
+值分别为 7，4，以及 1
+
+
+
+注意，输入的 "root" 和 "target" 实际上是树上的结点。
+上面的输入仅仅是对这些对象进行了序列化描述。
+ 
+
+提示：
+
+给定的树是非空的。
+树上的每个结点都具有唯一的值 0 <= node.val <= 500 。
+目标结点 target 是树上的结点。
+0 <= K <= 1000.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/all-nodes-distance-k-in-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// DFS + hashmap
+class Solution {
+    unordered_map<int, TreeNode*> parents;
+    vector<int> ans;
+
+    // 题外话：hashmap中没有存入root，当访问到root时，hashmap会自动存入[root.val, 0]，也就是认为root的父节点为NULL，恰好是满足的
+    void findParents(TreeNode* node) {
+        if (node->left != nullptr) {
+            parents[node->left->val] = node;
+            findParents(node->left);
+        }
+        if (node->right != nullptr) {
+            parents[node->right->val] = node;
+            findParents(node->right);
+        }
+    }
+
+    void findAns(TreeNode* node, TreeNode* from, int depth, int k) {
+        if (node == nullptr) {
+            return;
+        }
+        if (depth == k) {
+            ans.push_back(node->val);
+            return;
+        }
+        if (node->left != from) {
+            findAns(node->left, node, depth + 1, k);
+        }
+        if (node->right != from) {
+            findAns(node->right, node, depth + 1, k);
+        }
+        if (parents[node->val] != from) {
+            findAns(parents[node->val], node, depth + 1, k);
+        }
+    }
+
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        // 从 root 出发 DFS，记录每个结点的父结点
+        findParents(root);
+        // cout << parents.size() << endl; // 题外话：观察hashmap的大小变化
+
+        // 从 target 出发 DFS，寻找所有深度为 k 的结点
+        findAns(target, nullptr, 0, k);
+        // cout << parents.size() << endl; // 原本的hashmap没有存入root，访问了root之后，hashmap自动存入了该节点，hashmap的size比之前增加了1
+
+        return ans;
+    }
+};
