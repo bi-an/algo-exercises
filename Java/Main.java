2282ed5fd1526b6753a6c4ed26e08db602af0054
bi_an:desktop <2608120872@qq.com>
2021-08-01 22:51:34 +0800

    删除没用的Java文件夹

--- Java/Main.java
+++
@@ -1,8 +0,0 @@
-// package com;
-
-import com.oppo.Task;
-
-public class Main {
-  Task a = new Task();
-  a.fun();
-}
--- Java/com/oppo/Task.java
+++
@@ -1,7 +0,0 @@
-package com.oppo;
-
-public class Task {
-  public void fun(){
-    System.out.println("com.oppo.Task.fun");
-  }
-}
