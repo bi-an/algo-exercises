f77c421c5dbe615f6e56be85913227e39ad4aef0
bi-an <2608120872@qq.com>
2021-09-17 08:16:13 +0000

    add leecode/leetcode-cn/212.单词搜索II.java.

---
+++ leecode/leetcode-cn/212.单词搜索II.java
@@ -0,0 +1,67 @@
+// 方法一：字典树 + 回溯法
+class Solution {
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        Set<String> ans = new HashSet<String>();
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[0].length; ++j) {
+                dfs(board, trie, i, j, ans);
+            }
+        }
+
+        return new ArrayList<String>(ans);
+    }
+
+    public void dfs(char[][] board, Trie now, int i1, int j1, Set<String> ans) {
+        if (!now.children.containsKey(board[i1][j1])) {
+            return;
+        }
+        char ch = board[i1][j1];
+        // 解释一下Java的引用问题：
+        // 对于基本类型，都是值传递；对于引用类型，都是传递引用，但是引用本身是值传递，这相当于传入了C++的指针：
+        // 虽然所指对象相同，但是函数内外的引用变量本身其实是两个不同的变量。
+        // 这里now以引用传入，但是没有更改now所指向的内容，而是把now直接改指向为其他对象了
+        now = now.children.get(ch); // Java的引用
+        if (!"".equals(now.word)) {
+            ans.add(now.word);
+        }
+
+        board[i1][j1] = '#';
+        for (int[] dir : dirs) {
+            int i2 = i1 + dir[0], j2 = j1 + dir[1];
+            if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length) {
+                dfs(board, now, i2, j2, ans);
+            }
+        }
+        board[i1][j1] = ch;
+    }
+}
+
+class Trie {
+    String word;
+    Map<Character, Trie> children;
+    boolean isWord;
+
+    public Trie() {
+        this.word = "";
+        this.children = new HashMap<Character, Trie>();
+    }
+
+    public void insert(String word) {
+        Trie cur = this;
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word.charAt(i);
+            if (!cur.children.containsKey(c)) {
+                cur.children.put(c, new Trie());
+            }
+            cur = cur.children.get(c);
+        }
+        cur.word = word;
+    }
+}
