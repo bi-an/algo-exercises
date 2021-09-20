0d25c43b859e8ce80dd6878223f06e70302c6b1d
bi-an <2608120872@qq.com>
2021-09-20 03:23:56 +0000

    add leecode/leetcode-cn/650.只有两个键的键盘.cc.

---
+++ leecode/leetcode-cn/650.只有两个键的键盘.cc
@@ -0,0 +1,40 @@
+// 法一：动态规划
+class Solution {
+public:
+    int minSteps(int n) {
+        // dp[i]表示打印出i个A，需要的最少操作次数
+        // 要想打印出dp[i]，必须已有dp[j]，然后经过1次复制、若干次粘贴（即i/j次操作）得到
+        // dp[i] = min {dp[i], dp[j] + i/j}，当且仅当i能整除j时
+        vector<int> dp(n + 1, INT_MAX);
+        dp[1] = 0;
+
+        for(int i = 2; i < n + 1; i++) {
+            for(int j = 1; j <= i / 2; j++) { // 这里可以优化
+                if(i % j == 0)
+                    dp[i] = min(dp[i], dp[j] + i / j);
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+// 法一优化
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[1] = 0;
+
+        for(int i = 2; i < n + 1; i++) {
+            for(int j = 1; j * j <= i; j++) { // i一定可以同时整除i和i/j
+                if(i % j == 0) {
+                    dp[i] = min(dp[i], dp[j] + i / j);
+                    dp[i] = min(dp[i], dp[i / j] + j);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
