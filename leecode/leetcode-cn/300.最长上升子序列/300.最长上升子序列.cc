35b5df88dd3d0ed615d4d0fb11a0330a852e40e3
bi-an <2608120872@qq.com>
2021-09-21 08:00:18 +0000

    update leecode/leetcode-cn/300.最长上升子序列/300.最长上升子序列.cc.

--- leecode/leetcode-cn/300.最长上升子序列/300.最长上升子序列.cc
+++ leecode/leetcode-cn/300.最长上升子序列/300.最长上升子序列.cc
@@ -101,3 +101,35 @@ public:
         return len;
     }
 };
+
+// 二分查找写法二
+// https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int len = 1, n = (int)nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> d(n + 1, 0);
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+};
