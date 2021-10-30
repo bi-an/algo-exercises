2df53539f17897febffc07c1d2de04e295029e08
bi-an <2608120872@qq.com>
2021-10-30 12:50:02 +0000

    update luogu/P3368.【模板】树状数组2.cc.

--- luogu/P3368.【模板】树状数组2.cc
+++ luogu/P3368.【模板】树状数组2.cc
@@ -58,3 +58,56 @@ int main()
     }
   }
 }
+
+// @Athor zzg
+// @Date 2021/10/30
+#include <iostream>
+using namespace std;
+
+int n, m;
+int tree[500000];
+int input[500001];
+
+int lowbit(int x) {
+	return x & -x;
+}
+// A[x] + k
+void add(int x, int k) {
+	while (x <= n) {
+		tree[x] += k;
+		x += lowbit(x);
+	}
+}
+// [1,x]区间的前缀和
+int sum(int x) {
+	int ans = 0;
+	while (x > 0) {
+		ans += tree[x];
+		x -= lowbit(x);
+	}
+	return ans;
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> input[i];
+		add(i, input[i] - input[i - 1]); // tree存储差分数组
+	}
+
+	for (int i = 1; i <= m; i++) {
+		int a, b, c, k;
+		cin >> a;
+		if (a == 1) {
+			cin >> b >> c >> k;
+			add(b, k);
+			add(c + 1, -k);
+		}
+		else if (a == 2) {
+			cin >> b;
+			cout << sum(b) << endl;
+		}
+	}
+
+	return 0;
+}
