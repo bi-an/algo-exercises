6b1d86fa476060e90c169e4c99c83a1414fd87d5
bi-an <2608120872@qq.com>
2021-10-30 12:39:14 +0000

    update luogu/P3374.【模板】树状数组1.cc.

--- luogu/P3374.【模板】树状数组1.cc
+++ luogu/P3374.【模板】树状数组1.cc
@@ -55,3 +55,52 @@ int main()
       cout << sum(c) - sum(b - 1) << endl;
   }
 }
+
+// @Athor zzg
+// @Date 2021/10/30
+#include <iostream>
+using namespace std;
+
+int n, m;
+int tree[500000];
+
+int lowbit(int x) {
+	return x & -x;
+}
+// A[x] + k
+void add(int x, int k) {
+	while (x <= n) {
+		tree[x] += k;
+		x += lowbit(x);
+	}
+}
+// [1,x]区间的前缀和
+int sum(int x) {
+	int ans = 0;
+	while (x > 0) {
+		ans += tree[x];
+		x -= lowbit(x);
+	}
+	return ans;
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		int k;
+		cin >> k;
+		add(i, k);
+	}
+	for (int i = 1; i <= m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (a == 1) {
+			add(b, c);
+		}
+		else if (a == 2) {
+			cout << sum(c) - sum(b - 1) << endl;
+		}
+	}
+
+	return 0;
+}
