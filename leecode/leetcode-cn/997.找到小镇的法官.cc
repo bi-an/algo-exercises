c13d51f1b5b3135ea2bd395d9cb4ee7f96580d60
bi-an <2608120872@qq.com>
2021-12-19 20:28:01 +0800

    leetcode-cn/997.找到小镇的法官.cc

---
+++ leecode/leetcode-cn/997.找到小镇的法官.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        // 有向图的出度和入度
+        vector<int> inDegrees(n + 1);
+        vector<int> outDegrees(n + 1);
+
+        for(vector<int>& p : trust) {
+            outDegrees[p[0]]++;
+            inDegrees[p[1]]++;
+        }
+
+        int ans = -1;
+        for(int i=1;i<=n;i++) {
+            if(inDegrees[i] == n - 1 && outDegrees[i] == 0) {
+                ans = i;
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
