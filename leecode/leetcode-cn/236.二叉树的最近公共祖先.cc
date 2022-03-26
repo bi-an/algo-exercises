e094ce7a6f8c7992d56db59b4c24b219a0a877bf
bi_an:desktop <2608120872@qq.com>
2022-03-26 20:25:02 +0800

    leetcode-cn/236.二叉树的最近公共祖先.cc添加解法二

--- leecode/leetcode-cn/236.二叉树的最近公共祖先.cc
+++ leecode/leetcode-cn/236.二叉树的最近公共祖先.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-14 21:46:24
- * @LastEditTime: 2022-03-26 20:01:38
+ * @LastEditTime: 2022-03-26 20:23:51
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \practice\leecode\leetcode-cn\236.二叉树的最近公共祖先.cc
@@ -19,6 +19,7 @@ using namespace std;
  * };
  */
 
+// 递归
 // 注意：后序遍历保证这是“最近”的公共祖先
 class Solution
 {
@@ -40,7 +41,7 @@ class Solution
   }
 };
 
-// 写法二
+// 递归写法二
 class Solution {
     TreeNode* ans;
 public:
@@ -60,5 +61,50 @@ public:
         if ((inLeft && inRight) || (inRoot && (inLeft || inRight)))
             ans = root;
         return inLeft || inRight || inRoot;
+    }
+};
+
+// 方法二：记录父节点
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    unordered_map<TreeNode*, TreeNode*> fa; // 记录父节点
+    unordered_map<TreeNode*, bool> vis; // 记录访问过的父节点
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // 记录所有父节点
+        dfs(root);
+        fa[root] == nullptr;
+        // 从 p 出发，遍历所有父节点（注意包括自身）
+        while (p != nullptr) {
+            vis[p] = true;
+            p = fa[p];
+        }
+        // 从 q 出发，遍历所有父节点（注意包括自身）
+        // 则找到的第一个已经访问过的父节点是最近公共父节点
+        while (q != nullptr) {
+            if (vis[q])
+                return q;
+            q = fa[q];
+        }
+        return nullptr;
+    }
+
+    void dfs(TreeNode* root) {
+        if (root == nullptr)
+            return;
+        if (root->left)
+            fa[root->left] = root;
+        if (root->right)
+            fa[root->right] = root;
+        dfs(root->left);
+        dfs(root->right);
     }
 };
