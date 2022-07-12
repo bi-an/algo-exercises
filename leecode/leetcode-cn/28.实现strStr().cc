24b0cfb93caa91cea7682a958a9cabae129f1e95
bi_an:desktop <2608120872@qq.com>
2022-07-12 12:22:45 +0800

    KMP:leetcode-cn/28.实现strStr().cc

--- leecode/leetcode-cn/28.实现strStr().cc
+++ leecode/leetcode-cn/28.实现strStr().cc
@@ -1,11 +1,67 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
+class Solution
+{
+ public:
+  int strStr(string haystack, string needle)
+  {
+    if (needle == "")
+      return 0;
 
+    vector<int> next(needle.length());
+    fillNext(needle, next);
+    int i = 0, j = 0;
+    while (i < haystack.length())
+    {
+      if (haystack[i] != needle[j])
+      {
+        // 失配
+        if (j == 0)
+          ++i;
+        else
+          j = next[j];
+      }
+      else
+      {
+        // 匹配
+        ++i;
+        ++j;
+      }
+      if (j == needle.length())
+      {
+        return i - needle.length();
+      }
     }
+    return -1;
+  }
 
-    void fillNext(string& needle, vector<int>& next) {
-        assert(needle.length() == next.size() && needle.length() >= 2);
-        
+  void fillNext(string &pattern, vector<int> &next)
+  {
+    if (pattern.empty())
+      return;
+    assert(pattern.length() == next.size());
+
+    next[0] = -1;
+    if (pattern.length() == 1)
+      return;
+
+    next[1] = 0;
+    int pos = 2, cnd = next[1];
+    while (pos < pattern.length())
+    {
+      if (pattern[pos - 1] == pattern[cnd])
+      {
+        cnd = cnd + 1;
+        next[pos] = cnd;
+        pos = pos + 1;
+      }
+      else if (cnd > 0)
+      {
+        cnd = next[cnd];
+      }
+      else
+      {
+        next[pos] = 0; // 此时 cnd = 0
+        pos = pos + 1;
+      }
     }
+  }
 };
