12e346131335931b80744fab1b634980918db4a4
bi-an <2608120872@qq.com>
2022-07-12 23:26:06 +0800

    leetcode-cn/94.二叉树的中序遍历.cc

---
+++ leecode/leetcode-cn/94.二叉树的中序遍历.cc
@@ -0,0 +1,43 @@
+// 递归法
+// 时间复杂度 O(n)
+// 空间复杂度 O(n)
+// TODO
+
+// 迭代法
+// 时间复杂度 O(n)
+// 空间复杂度 O(n)
+class Solution
+{
+ public:
+  vector<int> inorderTraversal(TreeNode *root)
+  {
+    vector<int> res;
+    stack<TreeNode *> stk;
+    // Q: 能否先将 root 压栈，然后外部循环条件改为“栈非空”？
+    // A: 不能。虽然从 root 出发一路向左都压栈了，但是如果压栈后，栈内只有一个节点，
+    // 也就是没有左节点时，由于我们在本次循环内立即弹栈，导致下一次循环时栈为空，外部循环退出。
+    // 显然，如果此时还有右节点，外部循环还应该继续。
+    while (root != nullptr || !stk.empty())
+    {
+      // 从 root 出发一路向左，全部压栈
+      while (root != nullptr)
+      {
+        stk.push(root);
+        root = root->left;
+      }
+      root = stk.top();
+      stk.pop();
+      res.push_back(root->val);
+      // 即使 root->right 为 NULL 也必须赋值一次，
+      // 否则 root 永远不等于 NULL，导致 while 循环永不终止。
+      root = root->right;
+    }
+    return res;
+  }
+};
+
+// Morris 中序遍历
+// 时间复杂度 O(n)
+// 空间复杂度 O(1)
+// 参见 官方题解
+// TODO
