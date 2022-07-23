cb4990608dc68c562a12f512d44b9e2e799de679
bi_an:desktop <2608120872@qq.com>
2022-07-23 22:41:01 +0800

    leetcode-cn/111.二叉树的最小深度.cc

---
+++ leecode/leetcode-cn/111.二叉树的最小深度.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int leftDepth = minDepth(root->left);
+        int rightDepth = minDepth(root->right);
+        // 注意如下两种特殊情况：
+        if (leftDepth == 0)
+            return 1 + rightDepth;
+        if (rightDepth == 0)
+            return 1 + leftDepth;
+        return 1 + min(leftDepth, rightDepth);
+    }
+};
