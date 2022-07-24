1478f5ec428187f06de0ace5f98a145d4cf602f1
bi_an:desktop <2608120872@qq.com>
2022-07-24 19:04:21 +0800

    补充解法：Cadence/2022秋/两数之和.cc

--- 公司真题/Cadence/2022秋/两数之和.cc
+++ 公司真题/Cadence/2022秋/两数之和.cc
@@ -6,24 +6,60 @@
 using namespace std;
 
 int main() {
-    int x, y;
-    cin >> x;
-    string num = to_string(x);
-    if (x < 0) {
-        int i = 1;
-        for (; i < num.size(); ++i) {
-            if (num[i] > '6')
-                break;
-        }
-        cout << num.substr(0, i) + "6" + num.substr(i, num.size() - i) << endl;
-    } else {
-        int i = 0;
-        for (; i < num.size(); ++i) {
-            if (num[i] < '6')
-                break;
-        }
-        cout << num.substr(0, i) + "6" + num.substr(i, num.size() - i) << endl;
+	int m, N;
+	cin >> m >> N;
+	vector<int> nums(N);
+	map<int, int> cnt;
+	for (int i = 0; i < N; ++i) {
+		cin >> nums[i];
+		++cnt[nums[i]];
+	}
+	sort(nums.begin(), nums.end()); // 为了去重
+	long ans = 0;
+	int last = -1;
+	for (int i = 0; i < N; ++i) {
+		int x = nums[i];
+        // 去重
+		if (x > m / 2)
+			break;
+		if (x == last)
+			continue;
+		int y = m - x;
+		if (x == y) {
+			if (cnt[x] > 1)
+				ans += (long)cnt[x] * (cnt[x] - 1) / 2;
+		}
+		else {
+			ans += (long)cnt[x] * cnt[y];
+		}
+		last = x;
+	}
+	cout << ans << endl;
+
+	return 0;
+}
+
+
+// 方法二：去重不需要排序。
+long long countSum(vector<int>& nums, int m) {
+    long long ans = 0;
+
+    map<int, int> cnt;
+    for (int x : nums) {
+        ++cnt[x];
     }
-    
-    
+
+    for (int x : nums) {
+        int y = m - x;
+        // 去重
+        if (y > x || !cnt.count(y))
+            continue;
+        if (x == y) {
+            ans += (long long)cnt[x] * (cnt[x] - 1) / 2;
+        } else {
+            ans += (long long)cnt[x] * cnt[y];
+        }
+    }
+
+    return ans;
 }
