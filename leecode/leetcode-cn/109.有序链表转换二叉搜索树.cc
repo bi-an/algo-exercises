ae8bc3336250e63267d7eef746e2eb6a7e05bef7
bi-an <2608120872@qq.com>
2022-07-25 22:38:40 +0800

    更新注释:leetcode-cn/109.有序链表转换二叉搜索树.cc

--- leecode/leetcode-cn/109.有序链表转换二叉搜索树.cc
+++ leecode/leetcode-cn/109.有序链表转换二叉搜索树.cc
@@ -28,7 +28,7 @@ class Solution
   }
 };
 
-// 方法二：分治法，不使用额外数组保存链表
+// 方法二：分治法，每次都使用双指针找到链表中点。
 // 时间复杂度：O(nlogn)。证明方法见图解。TODO
 // 空间复杂度：O(1)。
 class Solution
@@ -68,7 +68,57 @@ class Solution
 };
 
 // 方法三：分治法 + 中序遍历优化
-// 把有序链表看成完全右倾的二叉搜索树，中序遍历时，上一次遍历到的节点就是本次
+// 时间复杂度：O(n)，一次遍历。
+// 空间复杂度：O(logn)，栈深度取决于二叉树的高度。
+class Solution
+{
+ public:
+  TreeNode *sortedListToBST(ListNode *head)
+  {
+    int len = getLength(head);
+    return sortedListToBST(head, 0, len - 1);
+  }
+
+  // 中序遍历范围为 [left, right] 的子链表，将其构建成一棵二叉搜索树。
+  // 该函数有两个返回值：
+  //    (1) return 构建好的二叉树；
+  //    (2) 通过 head 引用参数返回子链表 [left, right] 最后一个节点的下一个节点。
+  //    由于链表是有序的，所以 right 的下一个节点就是下一个更大的数字。
+  //    所以，返回的 head 恰好用来构建 [left, mid-1] 的二叉搜索树的根。
+  TreeNode *sortedListToBST(ListNode *&head, int left, int right)
+  {
+    if (left > right)
+      return nullptr;
+    int mid = (left + right) / 2;
+    TreeNode *root = new TreeNode(); // 占位节点
+    // 创建 root 的左子树并更新 head 为链表的下一个节点。
+    root->left = sortedListToBST(head, left, mid - 1);
+    // 更新根节点值
+    root->val = head->val;
+    // 创建右子树
+    // 必须更新 head 为下一个节点，仅仅通过将 head->next 作为传入本递归函数是不够的。
+    // 因为这意味着 head 在整个函数中都没有更新，还是传入时的值，
+    // 那么 head 指向的节点就不是子链表 [left, mid-1] 的下一个节点。
+    head = head->next; // 更新 head
+    // root->right = sortedListToBST(head->next, mid + 1, right); // 错误的做法！
+    root->right = sortedListToBST(head, mid + 1, right);
+
+    return root;
+  }
+
+  int getLength(ListNode *head)
+  {
+    int len = 0;
+    while (head)
+    {
+      ++len;
+      head = head->next;
+    }
+    return len;
+  }
+};
+
+// 方法三：分治法 + 中序遍历优化。写法二。
 // 时间复杂度：O(n)，一次遍历。
 // 空间复杂度：O(logn)，栈深度取决于二叉树的高度。
 class Solution
@@ -102,50 +152,6 @@ class Solution
     //（假装访问了当前的节点，准备访问下一个节点。）
     next_ = next_->next;
     root->right = sortedListToBST(mid + 1, right);
-    return root;
-  }
-
-  int getLength(ListNode *head)
-  {
-    int len = 0;
-    while (head)
-    {
-      ++len;
-      head = head->next;
-    }
-    return len;
-  }
-};
-
-// 方法三的递归函数的第二种写法，函数参数使用引用。
-class Solution
-{
- public:
-  TreeNode *sortedListToBST(ListNode *head)
-  {
-    int len = getLength(head);
-    return sortedListToBST(head, 0, len - 1);
-  }
-
-  // 每次调用，更新 next_ 为当前子树的下一个节点。
-  // head 为引用参数，是函数的返回值。保存的是链表的下一个节点。
-  TreeNode *sortedListToBST(ListNode *&head, int left, int right)
-  {
-    if (left > right)
-      return nullptr;
-    int mid = (left + right) / 2;
-    TreeNode *root = new TreeNode(); // 占位节点
-    // 创建 root 的左子树并更新 head 为链表的下一个节点。
-    root->left = sortedListToBST(head, left, mid - 1);
-    // 更新根节点值
-    root->val = head->val;
-    // 创建右子树
-    // 必须更新 head 为下一个节点，仅仅通过将 head->next 传入本递归函数是不够的。
-    // 因为这意味着 head 在整个函数中都没有更新，还是传入时的值，
-    // 那么 head 指向的节点就不是向链表左边部分的下一个节点。于是上面更新 root->val 的操作就是错误的。
-    head = head->next; // 必须更新 head 为下一个节点
-    root->right = sortedListToBST(head, mid + 1, right);
-
     return root;
   }
 
