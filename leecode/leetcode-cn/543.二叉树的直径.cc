0f5bfc9bfbb11300b194112ad3964fcd0f2b1b7a
bi-an <2608120872@qq.com>
2022-09-02 23:27:52 +0800

    修改注释:687.最长同值路径.cc;添加543.二叉树的直径.cc

---
+++ leecode/leetcode-cn/543.二叉树的直径.cc
@@ -0,0 +1,37 @@
+class Solution
+{
+  int res;
+
+ public:
+  int diameterOfBinaryTree(TreeNode *root)
+  {
+    res = 0;
+    dfs(root);
+    return res;
+  }
+
+  // @return 从 root 出发的路径长度
+  int dfs(TreeNode *root)
+  {
+    if (root == nullptr)
+      return 0;
+    int left = dfs(root->left);
+    int right = dfs(root->right);
+
+    if (root->left)
+      left = left + 1;
+    else
+      left = 0;
+
+    if (root->right)
+      right = right + 1;
+    else
+      right = 0;
+
+    // 用全局变量记录下最长直径
+    // （已经遍历过的所有节点中任意两点间距离最长的，可能包含 root，也可能不包含 root）
+    res = max(res, left + right);
+
+    return max(left, right);
+  }
+};
--- leecode/leetcode-cn/687.最长同值路径.cc
+++ leecode/leetcode-cn/687.最长同值路径.cc
@@ -1,28 +1,37 @@
 // 参考：[一篇文章解决所有二叉树路径问题](https://leetcode.cn/problems/longest-univalue-path/solution/yi-pian-wen-zhang-jie-jue-suo-you-er-cha-94j7/)
-class Solution {
-    int maxLen;
-public:
-    int longestUnivaluePath(TreeNode* root) {
-        maxLen = 0;
-        dfs(root);
-        return maxLen;
-    }
+class Solution
+{
+  int maxLen;
 
-    // 包含 root 的最长同值路径
-    int dfs(TreeNode* root) {
-        if (root == nullptr)
-            return 0;
-        int left = dfs(root->left);
-        int right = dfs(root->right);
-        if (root->left && root->val == root->left->val)
-            left = left + 1;
-        else
-            left = 0; // 一定要包含 root，因为下一次需要和当前树的父节点形成路径。
-        if (root->right && root->val == root->right->val)
-            right = right + 1;
-        else
-            right = 0;
-        maxLen = max(maxLen, left + right);
-        return max(left, right); // 只能返回最大同值深度，因为下一次需要和当前树的父节点形成路径。
-    }
+ public:
+  int longestUnivaluePath(TreeNode *root)
+  {
+    maxLen = 0;
+    dfs(root);
+    return maxLen;
+  }
+
+  // @return 从 root 出发的最长同值路径
+  int dfs(TreeNode *root)
+  {
+    if (root == nullptr)
+      return 0;
+    int left = dfs(root->left);
+    int right = dfs(root->right);
+
+    if (root->left && root->val == root->left->val)
+      left = left + 1;
+    else
+      left = 0; // 一定要包含 root，因为下一次需要和当前树的父节点形成路径。
+
+    if (root->right && root->val == root->right->val)
+      right = right + 1;
+    else
+      right = 0;
+
+    maxLen = max(maxLen, left + right);
+    return max(
+        left,
+        right); // 只能返回最大同值深度，因为下一次需要和当前树的父节点形成路径。
+  }
 };
