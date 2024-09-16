4e54ed4a7e34ab3e054e5c8f9c92d19f25faf0f7
bi-an <2608120872@qq.com>
2024-09-16 16:09:09 +0800

    重刷345.反转字符串中的元音字母.cc

--- leecode/leetcode-cn/345.反转字符串中的元音字母.cc
+++ leecode/leetcode-cn/345.反转字符串中的元音字母.cc
@@ -24,6 +24,7 @@ s 由 可打印的 ASCII 字符组成
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
+// 双指针，分别从左和从右遍历
 class Solution {
 public:
     string reverseVowels(string s) {
@@ -40,16 +41,40 @@ public:
         return s;
     }
 
-    // bool isVowel(char c) {
-    //     string vowels = "aeiouAEIOU";
-    //     for(char cc : vowels) {
-    //         if(cc == c) return true;
-    //     }
-    //     return false;
-    // }
-
     bool isVowel(char c) {
         string vowels = "aeiouAEIOU";
         return vowels.find(c) != std::string::npos;
     }
 };
+
+// 写法二，和上面代码本质一样
+namespace solution_2 {
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        // cpp中，后缀's'表示一个string类型的字面值常量
+        // 使用捕获列表的好处是，即使多次调用lambda函数也只会初始化一次，
+        // 并且在lambda函数内部不可变
+        // 捕获列表的生存期与lambda对象相同，其存储在lambda对象的stack上
+        auto isVowel = [vowels = "aeiouAEIOU"s](char c) -> bool {
+            return vowels.find(c) != string::npos;
+        };
+        int l = 0, r = (int)s.length() - 1;
+        while (l < r) {
+            if (isVowel(s[l]) && isVowel(s[r])) {
+                swap(s[l], s[r]);
+                ++l;
+                --r;
+            } else {
+                if (!isVowel(s[l]))
+                    ++l;
+                else
+                    --r;
+            }
+        }
+        return s;
+    }
+};
+
+}  // namespace solution_2
