9ece12aecfbf9aadbe08f4dc8623c099f1c07fd4
bi-an <2608120872@qq.com>
2024-11-15 19:56:05 +0800

    leetcode-cn/3239.最少翻转次数使二进制矩阵回文-i.cpp

---
+++ leecode/leetcode-cn/3239.最少翻转次数使二进制矩阵回文-i.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=3239 lang=cpp
+ *
+ * [3239] 最少翻转次数使二进制矩阵回文 I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        int ans1 = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n / 2; ++j) {
+                if (grid[i][j] != grid[i][n - 1 - j])
+                    ++ans1;
+            }
+        }
+
+        int ans2 = 0;
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < m / 2; ++i) {
+                if (grid[i][j] != grid[m - 1 - i][j])
+                    ++ans2;
+            }
+        }
+
+        return min(ans1, ans2);
+    }
+};
+// @lc code=end
