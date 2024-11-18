f951b8e3b71af63a6a0b251a0e85d816d059f2ad
bi-an <2608120872@qq.com>
2024-11-18 22:15:11 +0800

    将所有的cc后缀名改成cpp

--- leecode/leetcode-cn/303.区域和检索-数组不可变.cpp
+++ leecode/leetcode-cn/303.区域和检索-数组不可变.cpp
@@ -1,27 +1,29 @@
 /*
- * @lc app=leetcode.cn id=303 lang=cpp
- *
- * [303] 区域和检索 - 数组不可变
+ * @Author: your name
+ * @Date: 2022-03-26 17:16:51
+ * @LastEditTime: 2022-03-26 17:17:16
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \practice\leecode\leetcode-cn\303. 区域和检索 - 数组不可变.cc
  */
 
-// @lc code=start
-// sumRange()时间复杂度O(1), 空间复杂度O(n)
+// 前缀和
 class NumArray {
+    vector<long> preSum;
 public:
-    NumArray(vector<int> &nums) {
-        dp.resize(nums.size() + 1);
-        dp[0] = 0;
-        for (int i = 1; i < dp.size(); i++) {
-            dp[i] = dp[i - 1] + nums[i - 1];
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        preSum.resize(n + 1);
+        preSum[0] = 0;
+        // preSum[i] 为 nums[0], ..., nums[i-1] 之和
+        for (int i = 1; i <= n; ++i) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
         }
     }
-
-    int sumRange(int i, int j) {
-        return dp[j + 1] - dp[i];
+    
+    int sumRange(int left, int right) {
+        return preSum[right + 1] - preSum[left];
     }
-
-private:
-    std::vector<int> dp;
 };
 
 /**
@@ -29,4 +31,3 @@ private:
  * NumArray* obj = new NumArray(nums);
  * int param_1 = obj->sumRange(left,right);
  */
-// @lc code=end
