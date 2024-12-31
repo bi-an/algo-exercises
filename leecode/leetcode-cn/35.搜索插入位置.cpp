db71bf0f33a9052527760df074e565293d643a29
bi-an <2608120872@qq.com>
2024-12-31 13:34:12 +0800

    35.搜索插入位置.cpp

---
+++ leecode/leetcode-cn/35.搜索插入位置.cpp
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=cpp
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return ranges::lower_bound(nums, target) - nums.begin();
+    }
+};
+// @lc code=end
