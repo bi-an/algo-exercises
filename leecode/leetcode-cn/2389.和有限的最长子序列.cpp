729ab3055f09b0008ec9380a53901acd5e3dfcf2
bi-an <2608120872@qq.com>
2024-12-31 20:42:33 +0800

    2389.和有限的最长子序列.cpp

--- leecode/leetcode-cn/2389.和有限的最长子序列.cpp
+++ leecode/leetcode-cn/2389.和有限的最长子序列.cpp
@@ -4,9 +4,10 @@
  * [2389] 和有限的最长子序列
  */
 
-// @lc code=start
 // 贪心：每次选最小的，可以得到最大长度
 // 排序 + 双指针
+// 时间复杂度：O(nlogn + mlogm)
+// 空间复杂度：O(m)
 class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
@@ -30,4 +31,35 @@ public:
         return ans;
     }
 };
+
+namespace solution_2 {
+
+// @lc code=start
+// 官方题解：https://leetcode.cn/problems/longest-subsequence-with-limited-sum/solutions/2172081/he-you-xian-de-zui-chang-zi-xu-lie-by-le-xqox/
+// 贪心：每次选最小的，可以得到最大长度
+// 排序 + 前缀和 + 二分
+// 时间复杂度：O((n+m)logn)
+// 空间复杂度：O(n)
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        ranges::sort(nums);
+
+        int n = nums.size(), m = queries.size();
+        vector<int> ans(m), f(n + 1);  // f 表示 nums 的前缀和
+
+        for (int i = 0; i < n; ++i)
+            f[i + 1] = f[i] + nums[i];
+
+        int j = 0;
+        for (auto x : queries) {
+            auto i = ranges::upper_bound(f, x) - f.begin();
+            ans[j++] = i - 1;  // 前缀和数组长度为 n+1
+        }
+
+        return ans;
+    }
+};
 // @lc code=end
+
+}  // namespace solution_2
